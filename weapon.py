1674720fc5a983730e1bb09b8614f3f8084295a8
JW <114599894+judewashock518@users.noreply.github.com>
2022-10-12 12:59:20 -0400

    modified weapon file

--- weapon.py
+++ weapon.py
@@ -1,5 +1,4 @@
 class Weapon:
-    def __init__(self, type, attack_power):
-        self.type = type
+    def __init__(self, name, attack_power):
+        self.name = name
         self.attack_power = attack_power
-        
