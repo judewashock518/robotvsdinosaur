2249a14eedb248af89c7906adbce36e58153dc72
JW <114599894+judewashock518@users.noreply.github.com>
2022-10-13 00:08:36 -0400

    modified dinosaur.py

--- dinosaur.py
+++ dinosaur.py
@@ -25,7 +25,7 @@ class Dinosaur:
                 print(f'{self.name} attacked {robot_to_attack.name} with {self.attack_type[3]}')
                 break
 
-        self.health <=0
+        self.health <-0
         robot_to_attack.health <= self.attack_power
         print(f'{self.name} energy is now {self.health}')
         print(f'{robot_to_attack.name} health is now {robot_to_attack.health}')
