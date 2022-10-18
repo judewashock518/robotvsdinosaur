8789f70da16f3ad910254e91a82633068b64443e
JW <114599894+judewashock518@users.noreply.github.com>
2022-10-17 21:47:03 -0400

    added class functions to battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -2,6 +2,7 @@ import dinosaur
 import robot
 from fleet import Fleet
 from herd import Herd
+import random
 
 class Battlefield():
     def __init__(self):
@@ -23,3 +24,27 @@ class Battlefield():
         print('In order to declare a winner, one or the other has to have 0 health left.')
         print('Good luck!')
 
+
+    def choose_team(self):
+        choose_team = int(input('Choose your team: (1) Robots or (2) Dinosaurs'))
+        if choose_team == 1:
+            print('You chose the Robot fleet!')
+            return choose_team
+        elif choose_team == 2:
+            print('You chose the herd of Dinosaurs!')
+            return choose_team
+        else:
+            print('Try input again.')
+            self.choose_team()
+    
+
+    def battle(self):
+        first_turn = random.randint(1, 2)
+        if first_turn == 1:
+            print('Robots go first!')
+            first_turn = 1
+        if first_turn == 2:
+            print('Dinosaurs go first!')
+            first_turn = 2
+
+        
