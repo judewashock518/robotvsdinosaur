f31793594de8e453e4fd6a2d891b5a91f2aef8e0
JW <114599894+judewashock518@users.noreply.github.com>
2022-10-19 14:50:05 -0400

    added self.weapon variables within attack_dinosaur function on robot.py

--- robot.py
+++ robot.py
@@ -1,3 +1,5 @@
+from weapon import Weapon
+
 class Robot:
     def __init__(self, name, Weapon):
         self.name = name
@@ -14,24 +16,28 @@ class Robot:
                 except ValueError:
                     continue
                 if weapon_choice == 1:
+                    self.weapon = Weapon("Sword", 8)
                     print(f'{self.name} attacked {dinosaur_to_attack.name} with {self.weapon_choice[0]}')
                     dinosaur_to_attack.health -= self.weapon.attack_power
                     print(f'{self.name} energy is now {self.health}')
                     print (f'{dinosaur_to_attack.name} health is now {dinosaur_to_attack.health}')
                     break
                 if weapon_choice == 2:
+                    self.weapon = Weapon("Electrical Orb", 20)
                     print(f'{self.name} attacked {dinosaur_to_attack.name} with {self.weapon_choice[1]}')
                     dinosaur_to_attack.health -= self.weapon.attack_power
                     print(f'{self.name} energy is now {self.health}')                    
                     print (f'{dinosaur_to_attack.name} health is now {dinosaur_to_attack.health}')
                     break 
                 if weapon_choice == 3:
+                    self.weapon = Weapon("Laser Beams", 30)
                     print(f'{self.name} attacked {dinosaur_to_attack.name} with {self.weapon_choice[2]}')
                     dinosaur_to_attack.health -= self.weapon.attack_power
                     print(f'{self.name} energy is now {self.health}')
                     print (f'{dinosaur_to_attack.name} health is now {dinosaur_to_attack.health}')
                     break
                 if weapon_choice == 4:
+                    self.weapon = Weapon("Taser", 50)
                     print(f'{self.name} attacked {dinosaur_to_attack.name} with {self.weapon_choice[3]}')
                     dinosaur_to_attack.health -= self.weapon.attack_power
                     print(f'{self.name} energy is now {self.health}')
